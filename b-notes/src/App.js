a1cd126f39a5c6607198162fceb5e3f03dad3802
JoseH44 <jrdjhc45@gmail.com>
2021-06-14 21:51:54 -0600

    creacion de los botones para registrarse e iniciar sesión

--- b-notes/src/App.js
+++ b-notes/src/App.js
@@ -2,25 +2,25 @@ import logo from "./logo.svg";
 import "./App.css";
 import firebase from "firebase";
 import { StyledFirebaseAuth } from "react-firebaseui";
+import React, { useState } from "react";
+import { render } from "@testing-library/react";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="Wrap">
+          <div className="item">
+            <button className="botonL">Iniciar Sesión</button>
+          </div>
+          <div className="item">
+            <button className="botonR">Registrarse</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
