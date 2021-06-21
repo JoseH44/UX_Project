2f832a953e5dc73ffccfffe6b531e944c2778ce0
JoseH44 <jrdjhc45@gmail.com>
2021-06-21 16:59:04 -0600

    listado de las notas de los demás y las mías

--- b-notes/src/Components/Main.js
+++ b-notes/src/Components/Main.js
@@ -7,6 +7,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import { useEffect, useState } from "react";
+import "../Styles/Main.css";
 
 import {
   FirebaseAppProvider,
@@ -31,7 +32,11 @@ const Main = (props) => {
   const [contenido, setContenido] = useState("");
   const [etiqueta, setEtiqueta] = useState("");
   const [likes, setLikes] = useState(0);
-  const [dislikes, setDislikes] = useState("");
+  const [dislikes, setDislikes] = useState(0);
+  const [option, setoption] = useState("");
+  const [changeEtiqueta, setChangeEtiqueta] = useState();
+  const [alreadyLiked, setAlreadyLiked] = useState(false);
+  const [alreadyDisliked, setAlreadyDisliked] = useState(false);
   //variables del usuario
   var userID = props.uid;
   var userName = props.displayName;
@@ -50,15 +55,73 @@ const Main = (props) => {
       dislikes,
       etiqueta,
       likes,
+      userID,
+      fecha_creada: firebase.firestore.Timestamp.fromDate(new Date()),
     });
 
     setEtiqueta("");
     setContenido("");
   }
 
-  function verification() {
-    console.log(props.displayName);
+  var db = firebase.firestore();
+  const increment = firebase.firestore.FieldValue.increment(1);
+  const decrement = firebase.firestore.FieldValue.increment(-1);
+
+  function edit(docApunte) {
+    const notaRef = db.collection("Apuntes").doc(docApunte);
+    notaRef.update({ contenido: contenido, etiqueta: etiqueta });
   }
+  //funcion para incrementar el like
+  function incrementLike(docA) {
+    if (alreadyLiked) {
+      const postRefG = db.collection("Apuntes").doc(docA);
+      postRefG.update({ likes: decrement });
+      setAlreadyLiked(false);
+    } else {
+      const postRef = db.collection("Apuntes").doc(docA);
+      //setLikedArray();
+      postRef.update({ likes: increment });
+
+      setAlreadyLiked(true);
+    }
+  }
+
+  //funcion para incrementar el dislike
+  function incrementDisLike(docA) {
+    if (alreadyDisliked) {
+      const postRefG = db.collection("Apuntes").doc(docA);
+      postRefG.update({ dislikes: decrement });
+      setAlreadyDisliked(false);
+    } else {
+      const postRef = db.collection("Apuntes").doc(docA);
+      postRef.update({ dislikes: increment });
+      setAlreadyDisliked(true);
+    }
+  }
+
+  function verification1() {
+    setoption("global");
+  }
+
+  function verification2() {
+    setoption("mine");
+  }
+
+  //notas que no son mías
+  const notesCollection = useFirestore()
+    .collection("Apuntes")
+    .where("userID", "!=", props ? props.uid : null);
+  //collection
+  const { status: statusNotesCollection, data: dataNotesCollection } =
+    useFirestoreCollectionData(notesCollection);
+
+  //notas que son mías
+  const misNotas = useFirestore()
+    .collection("Apuntes")
+    .where("userID", "==", props ? props.uid : null);
+  //colección
+  const { status: statMisNotasCollection, data: dataMisNotasCollection } =
+    useFirestoreCollectionData(misNotas);
 
   return (
     <>
@@ -67,13 +130,14 @@ const Main = (props) => {
           <Navbar.Brand href="#home">
             <h2>B-notes</h2>
           </Navbar.Brand>
+
           <Nav className="mr-auto">
-            <Nav.Link onClick={verification} href="">
-              Principal
-            </Nav.Link>
-            <Nav.Link onClick={verification} href="">
+            <button onClick={() => verification1()} className="nav-button">
+              Menú
+            </button>
+            <button onClick={() => verification2()} className="nav-button">
               Mis Notas
-            </Nav.Link>
+            </button>
             <Nav.Item className="justify-content-end">
               <Button className="" onClick={signOut} variant="primary">
                 Cerrar Sesión
@@ -111,11 +175,69 @@ const Main = (props) => {
             </Form>
             <br />
             <Button onClick={guardar} variant="primary">
-              Guardar
+              Crear
             </Button>{" "}
             <br />
           </Col>
         </div>
+      </div>
+      <div>
+        {option === "global" ? (
+          <div>
+            {dataNotesCollection.map((note) => (
+              <div className="post" key={note.NO_ID_FIELD}>
+                <h6>Post por: {note.userName}</h6>
+                <h4>{note.etiqueta}</h4>
+                <br />
+                <p>{note.contenido}</p>
+
+                <br />
+                <button onClick={() => incrementLike(note.NO_ID_FIELD)}>
+                  {note.likes}
+                </button>
+                <button onClick={() => incrementDisLike(note.NO_ID_FIELD)}>
+                  {note.dislikes}
+                </button>
+                <br />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>
+            {statMisNotasCollection === "success" &&
+              dataMisNotasCollection.map((myNote) => (
+                <div className="container">
+                  <div className="parent-div2">
+                    <h3>¡Mi Apunte!</h3>
+                    <Col sm={10}>
+                      <Form>
+                        <Form.Group controlId="exampleForm.ControlInput1">
+                          <br />
+                          <Form.Control type="text" value={myNote.etiqueta} />
+                          <br />
+                        </Form.Group>
+
+                        <Form.Group controlId="exampleForm.ControlTextarea1">
+                          <Form.Control
+                            value={myNote.contenido}
+                            as="textarea"
+                            rows={3}
+                          />
+                        </Form.Group>
+                        <button>{myNote.likes}</button>
+                        <button>{myNote.dislikes}</button>
+                      </Form>
+                      <br />
+                      <Button onClick={guardar} variant="primary">
+                        Guardar
+                      </Button>{" "}
+                      <br />
+                    </Col>
+                  </div>
+                </div>
+              ))}
+          </div>
+        )}
       </div>
     </>
   );
